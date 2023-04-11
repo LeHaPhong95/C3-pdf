122b3da1e35b724c89e54896b2bd3a400465684a
Phong Le <phonglh1002@gmail.com>
2023-04-11 13:54:52 +0700

    update ui]

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,21 @@
+import { Link } from 'react-router-dom'
+
 function Header() {
   return (
     <div className="sticky top-0 bg-[#FFF]">
       <div className=" flex items-center justify-between h-[165px] w-[1440px] m-auto px-28 border-b-2 border-b-[#DEDEDE] ">
-        <div className="flex items-center">
+        <Link to="/" className="flex items-center">
           <img src="img/logo.png" alt="" />
           <span className="text-4xl font-bold pl-5">PDF TOOLS</span>
-        </div>
-        <div className="bg-gradient-to-l  to-[#2259C9] from-[#00BAEB] flex justify-around items-center w-[333px] rounded-full p-3 h-[60px]">
-          <img src="img/upload.png" alt="" />
-          <button className="text-lg font-bold text-[#FFFFFF]">
-            Upload File To Convert PDF
-          </button>
+        </Link>
+
+        <div className="bg-gradient-to-l  to-[#2259C9] from-[#00BAEB]  w-[333px] rounded-full p-3 h-[60px]">
+          <Link to="/merge" className="flex justify-around items-center">
+            <img src="img/upload.png" alt="" />
+            <button className="text-lg font-bold text-[#FFFFFF]">
+              Upload File To Convert PDF
+            </button>
+          </Link>
         </div>
       </div>
     </div>
--- src/page/Merge/index.js
+++ src/page/Merge/index.js
@@ -10,11 +10,11 @@ function Merge() {
           </span>
         </div>
         <div className="flex justify-end -mt-[280px]">
-          <img src="img/Group.png"alt=""  />
+          <img src="img/Group.png" alt="" />
         </div>
       </div>
       <div className="-mt-52 flex justify-start">
-        <img className="-z-10" src="img/l.png"alt=""  />
+        <img className="-z-10" src="img/l.png" alt="" />
       </div>
       <div className="flex justify-end -mt-52">
         <img className="-z-10" src="img/r.png" alt="" />
@@ -22,9 +22,9 @@ function Merge() {
       <div className="text-center -mt-[450PX]">
         <p className="text-6xl font-bold ">Most Popular PDF Tools</p>
       </div>
-      <div className=" z-10 text-center mb-44 shadow-[0_4px_24px_rgba(0,0,0,0.1)] w-[1220px] bg-[#FFFFFF] m-auto mt-12">
+      <div className=" z-10 text-center mb-44 shadow-[0_4px_24px_#F9C067] w-[1220px] bg-[#FFFFFF] m-auto mt-12">
         <div className="flex items-center justify-center mb-5 pt-12">
-          <img src="img/merge.png"alt=""  />
+          <img src="img/merge.png" alt="" />
           <p className="text-2xl font-bold ml-9">Merge PDF</p>
         </div>
         <p className="text-lg font-semibold text-[#9B9B9B]">
@@ -36,11 +36,11 @@ function Merge() {
             <p>File name abcxyz. pdf</p>
           </div>
           <div>
-            <img src="img/img2.png"alt=""  />
+            <img src="img/img2.png" alt="" />
             <p>File name abcxyz. pdf</p>
           </div>
           <div>
-            <img src="img/img3.png"alt=""  />
+            <img src="img/img3.png" alt="" />
             <p>File name abcxyz. pdf</p>
           </div>
           <div>
@@ -53,7 +53,7 @@ function Merge() {
             <img src="img/file.png" alt="" />
             <p className="text-[#FFFFFF] text-2xl font-bold">Choose files</p>
             <div className="w-1 h-7 bg-[#FFFFFF] " />
-            <img src="img/Down.png"alt=""  />
+            <img src="img/Down.png" alt="" />
           </button>
           <p className="text-lg font-semibold mt-6">
             Or drag and drop file in this space
@@ -62,6 +62,32 @@ function Merge() {
         <button className="bg-[#2259C9] py-4 px-14 rounded text-[#FFFFFF] text-2xl font-bold mb-12 mt-5">
           Merge
         </button>
+      </div>
+
+      <div className="w-[500px] m-auto mb-40">
+        <p className="text-2xl font-bold">
+          PDF has merged your files into a single PDF
+        </p>
+        <div className="flex text-lg justify-center font-bold  border-dashed border-2 border-[#2259C9]  p-5 my-8">
+          <img src="img/edit.png" alt="" />
+          <p className="text-[#2259C9] ml-3">
+            Loremipsumissimplytext_merged.pdf - 222.5KB
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <button className="bg-[#2259C9] w-[153px] flex items-center rounded-full justify-center py-4">
+            <img src="img/download.png" alt="" />
+            <p className="ml-2 text-[#FFFFFF] text-sm font-bold">Download</p>
+          </button>
+          <button className="bg-[#22C96F] w-[153px] flex items-center rounded-full justify-center py-4">
+            <img src="img/preview.png" alt="" />
+            <p className="ml-2 text-[#FFFFFF] text-sm font-bold">Preview</p>
+          </button>
+          <button className="bg-[#C92268] w-[153px] flex items-center rounded-full justify-center py-4">
+            <img src="img/restart.png" alt="" />
+            <p className="ml-2 text-[#FFFFFF] text-sm font-bold">Restart</p>
+          </button>
+        </div>
       </div>
     </div>
   )
